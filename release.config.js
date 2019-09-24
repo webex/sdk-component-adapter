1cdc0f8101b1254ecb355f898775dd9277ffd499
Arash Koushkebaghi <arash.koush@gmail.com>
2019-09-24 14:15:12 -0400

    build(semantic-release): configuration

---
+++ release.config.js
@@ -0,0 +1,50 @@
+/*
+ * we use semantic-release to automate the whole
+ * package release workflow and the options,
+ * mode and plugins can be set here
+ * more info: https://github.com/semantic-release/semantic-release
+ */
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+      },
+    ],
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: 'package.json',
+            label: 'Package JSON',
+          },
+          {
+            path: 'dist/webexSDKComponentAdapter.cjs.js',
+            label: 'Common JS distribution',
+          },
+          {
+            path: 'dist/webexSDKComponentAdapter.umd.js',
+            label: 'Universal Module Definition JS distribution',
+          },
+          {
+            path: 'dist/webexSDKComponentAdapter.esm.js',
+            label: 'ECMAScript module JS distribution',
+          },
+        ],
+      },
+    ],
+  ],
+};
