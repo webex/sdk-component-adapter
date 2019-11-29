7c883b6b14f23dbb4033f0eafebebb2451ba878b
Arash Koushkebaghi <arash.koush@gmail.com>
2019-11-29 01:20:39 -0800

    build(JestPup): reconfigure puppeteer

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   server: {
     command: `npm run serve`,
-    port: 4444,
+    port: 1234,
   },
 };
--- scripts/puppeteer.server.js
+++
@@ -1,9 +0,0 @@
-import * as http from 'http';
-
-http
-  .createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Hello World!');
-    response.end();
-  })
-  .listen(4444);
