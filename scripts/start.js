d9edd5703e31a47f8420444d2f428e0f1af14de6
lalli-flores <lalli.flores@gmail.com>
2020-11-30 15:55:53 -0800

    build(nodemon): set configuration in package.json
    This also updates the way dotenv is used; dotenv is now preloaded before CLI runs

--- scripts/start.js
+++ scripts/start.js
@@ -1,14 +1,10 @@
-import * as dotenv from 'dotenv';
 import Webex from 'webex';
 
 import WebexSDKAdapter from '../src/WebexSDKAdapter';
 
-// Allows the scripts to read the corresponding configuration
-dotenv.config({path: `${__dirname}/../.env.development`});
-
 if (!process.env.WEBEX_ACCESS_TOKEN) {
   // eslint-disable-next-line no-console
-  console.error('Please set a valid WEBEX_ACCESS_TOKEN environment in `env.development` file at the root level.');
+  console.error('Please set a valid WEBEX_ACCESS_TOKEN in `.env` file at the root level.');
 
   process.exit(1);
 }
