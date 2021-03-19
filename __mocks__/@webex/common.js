b450d466e9f9b61830070fe0e99d43863597a44e
Andrei Cociobea <acociobe@cisco.com>
2021-03-19 09:39:26 +0200

    feat(MembershipsSdkAdapter): return members of a room

--- __mocks__/@webex/common.js
+++ __mocks__/@webex/common.js
@@ -1,6 +1,17 @@
 export const deconstructHydraId = (ID) => {
   if (ID.includes('PEOPLE')) return {type: 'PEOPLE'};
   if (ID.includes('ROOM')) return {type: 'ROOM'};
+  if (ID.includes('personID')) return {id: ID};
+  if (ID.includes('organizationID')) return {id: ID};
 
   return {};
+};
+
+export const SDK_EVENT = {
+  EXTERNAL: {
+    EVENT_TYPE: {
+      CREATED: 'created',
+      DELETED: 'deleted',
+    },
+  },
 };
--- src/MembershipsSDKAdapter.js
+++ src/MembershipsSDKAdapter.js
@@ -1,5 +1,26 @@
-import {BehaviorSubject, throwError} from 'rxjs';
-import {DestinationType, MembershipsAdapter} from '@webex/component-adapter-interfaces';
+import {
+  BehaviorSubject,
+  concat,
+  defer,
+  fromEvent,
+  merge,
+  throwError,
+} from 'rxjs';
+import {
+  filter,
+  finalize,
+  map,
+  mergeMap,
+  publishReplay,
+  refCount,
+} from 'rxjs/operators';
+import {SDK_EVENT, deconstructHydraId} from '@webex/common';
+import {
+  DestinationType,
+  MembershipsAdapter,
+} from '@webex/component-adapter-interfaces';
+
+const MAX_MEMBERSHIPS = 5000; // maximum participants in a space
 
 // TODO: Figure out how to import JS Doc definitions and remove duplication.
 /**
@@ -38,6 +59,125 @@ export default class MembershipsSDKAdapter extends MembershipsAdapter {
     super(datasource);
 
     this.members$ = {}; // cache membership observables based on membership id
+    this.listenerCount = 0;
+  }
+
+  /**
+   * Tells the SDK to start listening to memberships events and tracks the amount of calls.
+   *
+   * Note: Since the SDK listens to ALL memberships events, this function only
+   * calls the SDK's `memberships.listen` function on the first membership to listen.
+   * Repeated calls to `memberships.listen` are not needed afterwards.
+   *
+   * @private
+   */
+  startListeningToMembershipsUpdates() {
+    if (this.listenerCount === 0) {
+      // Tell the sdk to start listening to membership changes
+      this.datasource.memberships.listen();
+    }
+    this.listenerCount += 1;
+  }
+
+  /**
+   * Tells the SDK to stop listening to memberships events.
+   *
+   * Note: Since the SDK listens to ALL memberships events, this function only
+   * calls the SDK's `memberships.stopListening` function once all of the listeners are done.
+   * If `memberships.stopListening` is called early, existing subscribers won't get any updates.
+   *
+   * @private
+   */
+  stopListeningToMembershipsUpdates() {
+    this.listenerCount -= 1;
+
+    if (this.listenerCount <= 0) {
+      // Once all listeners are done, stop listening
+      this.datasource.memberships.stopListening();
+    }
+  }
+
+  /**
+   * Returns an observable that emits room members list of the given roomID.
+   *
+   * @private
+   * @param {string} roomID ID of the room
+   * @returns {external:Observable.<Array.<Member>>} Observable stream that emits a list of current members in a room
+   */
+  getRoomMembers(roomID) {
+    this.startListeningToMembershipsUpdates();
+
+    const membershipToMember = (membership) => ({
+      id: deconstructHydraId(membership.personId).id,
+      orgID: deconstructHydraId(membership.personOrgId).id,
+      muted: null,
+      sharing: null,
+      inMeeting: null,
+    });
+
+    const members$ = defer(() => this.datasource.memberships.list({
+      roomId: roomID,
+      max: MAX_MEMBERSHIPS,
+    })).pipe(
+      map((page) => page.items.map(membershipToMember)),
+    );
+
+    const createdEvent$ = fromEvent(
+      this.datasource.memberships,
+      SDK_EVENT.EXTERNAL.EVENT_TYPE.CREATED,
+    );
+
+    const deletedEvent$ = fromEvent(
+      this.datasource.memberships,
+      SDK_EVENT.EXTERNAL.EVENT_TYPE.DELETED,
+    );
+
+    const event$ = merge(createdEvent$, deletedEvent$)
+      .pipe(
+        filter((event) => event.data.roomId === roomID),
+        mergeMap(() => members$),
+      );
+
+    return concat(members$, event$).pipe(
+      publishReplay(1),
+      refCount(),
+      finalize(() => {
+        this.stopListeningToMembershipsUpdates();
+      }),
+    );
+  }
+
+  /**
+   * Returns an observable that emits meeting members list of the given meetingID.
+   *
+   * @private
+   * @param {string} meetingID ID of the meeting
+   * @returns {external:Observable.<Array.<Member>>} Observable stream that emits a list of current members in a meeting
+   */
+  getMeetingMembers(meetingID) {
+    const meeting = this.datasource.meetings.getMeetingByType('id', meetingID);
+    let members$;
+
+    if (!meeting) {
+      members$ = throwError(new Error(`Meeting ${meetingID} not found.`));
+    } else {
+      const members = meeting.members
+        && meeting.members.membersCollection
+        && meeting.members.membersCollection.members;
+
+      // Behavior subject will keep the last emitted object for new subscribers
+      // https://rxjs.dev/guide/subject#behaviorsubject
+      members$ = new BehaviorSubject(getMembers(members));
+
+      // Emit on membership updates
+      meeting.members.on('members:update', (payload) => {
+        if (payload && payload.full) {
+          members$.next(getMembers(payload.full));
+        }
+      });
+    }
+
+    return members$;
   }
 
   /**
@@ -54,32 +194,19 @@ export default class MembershipsSDKAdapter extends MembershipsAdapter {
     let members$ = this.members$[membershipID];
 
     if (!members$) {
-      if (destinationType !== DestinationType.MEETING) {
-        members$ = throwError(new Error(`getMembersFromDestination for ${destinationType} is not currently supported.`));
-      } else {
-        const meeting = this.datasource.meetings.getMeetingByType('id', destinationID);
-
-        if (!meeting) {
-          members$ = throwError(new Error(`Meeting ${destinationID} not found.`));
-        } else {
-          const members = meeting.members
-            && meeting.members.membersCollection
-            && meeting.members.membersCollection.members;
-
-          // Behavior subject will keep the last emitted object for new subscribers
-          // https://rxjs.dev/guide/subject#behaviorsubject
-          members$ = new BehaviorSubject(getMembers(members));
-
-          this.members$[membershipID] = members$; // save for future calls
-
-          // Emit on membership updates
-          meeting.members.on('members:update', (payload) => {
-            if (payload && payload.full) {
-              members$.next(getMembers(payload.full));
-            }
-          });
-        }
+      switch (destinationType) {
+        case DestinationType.ROOM:
+          members$ = this.getRoomMembers(destinationID);
+          break;
+        case DestinationType.MEETING:
+          members$ = this.getMeetingMembers(destinationID);
+          break;
+        default:
+          members$ = throwError(new Error(`getMembersFromDestination for ${destinationType} is not currently supported.`));
       }
+
+      // save for future calls
+      this.members$[membershipID] = members$;
     }
 
     return members$;
