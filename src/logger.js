9f88804932c893dc450e1e8f2f15f27f46402d4f
Patricia Mihut <pmihut@cisco.com>
2021-07-15 13:11:59 +0300

    feat(package): configure winston logger

---
+++ src/logger.js
@@ -0,0 +1,18 @@
+import {createLogger, format, transports} from 'winston';
+
+const activeTransports = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  activeTransports.push(new transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.simple(),
+    ),
+  }));
+}
+
+const logger = createLogger({
+  transports: activeTransports,
+});
+
+export default logger;
