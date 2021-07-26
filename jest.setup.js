67ac57faee47990f25b2cebb537ec88243a09e80
bogdan.maier <bogdan.maier@endava.com>
2021-07-26 10:54:37 +0300

    fix(MeetingsSdkAdapter): refactor tests to use global MediaStream mock object

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,60 @@
 // Mock Web Media APIs
 
-global.MediaStream = jest.fn();
-global.MediaStream.prototype = {
-  getTracks: jest.fn(() => []),
-  removeTrack: jest.fn(),
-};
+global.MediaStream = jest.fn((tracksOrStream = []) => {
+  let tracks;
+
+  if (tracksOrStream instanceof global.MediaStream) {
+    tracks = [...tracksOrStream.getTracks()];
+  } else {
+    tracks = tracksOrStream;
+  }
+
+  for (const track of tracks) {
+    if (!('enabled' in track)) {
+      track.enabled = true;
+    }
+
+    if (!('stop' in track)) {
+      Object.defineProperty(track, 'stop', {
+        value: jest.fn(),
+        enumerable: false,
+      });
+    }
+  }
+
+  return {
+    getTracks: jest.fn(() => tracks),
+    getAudioTracks: jest.fn(() => tracks.filter((track) => track.kind === 'audio')),
+    getVideoTracks: jest.fn(() => tracks.filter((track) => track.kind === 'video')),
+    removeTrack: jest.fn((toRemove) => tracks.filter((track) => track !== toRemove)),
+  };
+});
+
+expect.extend({
+  /**
+   * Custom jest matcher that checks that two media streams have the same tracks
+   *
+   * @param {MediaStream} received
+   * @param {MediaStream} expected
+   * @returns {object}
+   */
+  toMatchMediaStream(received, expected) {
+    const options = {
+      comment: 'Object.is equality',
+      isNot: this.isNot,
+      promise: this.promise,
+    };
+
+    const pass = this.equals(received.getTracks(), expected.getTracks());
+
+    const message = () => `${this.utils.matcherHint('toMatchMediaStream', undefined, undefined, options)}\n
+Expected: ${this.utils.printExpected(expected && expected.getTracks())}
+Received: ${this.utils.printReceived(received && received.getTracks())}`;
+
+    return {
+      actual: received,
+      message,
+      pass,
+    };
+  },
+});
