8bc2ab401bbd09aa3ebd0f9c72d7b673950bffc7
Taymoor Khan <tmkhan.k2@gmail.com>
2022-02-02 11:55:50 -0800

    feat(rooms): add getRoomActivities method

--- __mocks__/@webex/common.js
+++ __mocks__/@webex/common.js
@@ -3,6 +3,7 @@ export const deconstructHydraId = (ID) => {
   if (ID.includes('ROOM')) return {type: 'ROOM'};
   if (ID.includes('personID')) return {type: 'PEOPLE', id: ID, cluster: 'us'};
   if (ID.includes('organizationID')) return {type: 'ORGANIZATION', id: ID, cluster: 'us'};
+  if (ID.includes('123')) return {type: 'ROOM', id: ID, cluster: 'us'};
 
   return {};
 };
