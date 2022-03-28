fec1465d97cab748c55966f7b06c84a332dbbf6d
harsh <harsh789rai@gmail.com>
2022-03-28 20:15:49 +0530

    feat(PeopleSdkAdapter): add search people method

--- src/PeopleSDKAdapter.test.js
+++ src/PeopleSDKAdapter.test.js
@@ -7,11 +7,13 @@ describe('People SDK Adapter', () => {
   let mockSDK;
   let peopleSDKAdapter;
   let personID;
+  let query;
 
   beforeEach(() => {
     mockSDK = createMockSDK();
     peopleSDKAdapter = new PeopleSDKAdapter(mockSDK);
     personID = 'personID';
+    query = 'query';
   });
 
   describe('getMe()', () => {
@@ -116,6 +118,58 @@ describe('People SDK Adapter', () => {
 
       subscription.unsubscribe();
       expect(mockSDK.internal.presence.unsubscribe).toHaveBeenCalled();
+    });
+  });
+
+  describe('searchPeople', () => {
+    test('returns an observable', () => {
+      expect(isObservable(peopleSDKAdapter.searchPeople(query))).toBeTruthy();
+    });
+
+    test('emits persons list on subscription', (done) => {
+      mockSDK.people.list = jest.fn(() => Promise.resolve({
+        items: [
+          {
+            id: 'personIDCurrentUser',
+            emails: ['email@cisco.com'],
+            displayName: 'Webex Components',
+            firstName: 'Webex',
+            lastName: 'Components',
+            nickName: 'Webex',
+            avatar: 'avatar',
+            orgId: 'orgID',
+            status: 'active',
+          },
+        ],
+      }));
+
+      peopleSDKAdapter.searchPeople(query).subscribe((peopleList) => {
+        expect(peopleList).toEqual([{
+          ID: 'personIDCurrentUser',
+          emails: ['email@cisco.com'],
+          displayName: 'Webex Components',
+          firstName: 'Webex',
+          lastName: 'Components',
+          nickName: 'Webex',
+          avatar: 'avatar',
+          orgID: 'orgID',
+          status: 'active',
+        }]);
+      });
+      done();
+    });
+
+    test('emits an error if the SDK fails to fetch the person list', (done) => {
+      const errorMsg = 'error fetching person list';
+
+      mockSDK.people.list = jest.fn(() => Promise.reject(new Error(errorMsg)));
+      peopleSDKAdapter.searchPeople(query).subscribe(
+        () => {},
+        (error) => {
+          expect(error.message).toBe(errorMsg);
+          done();
+        },
+      );
     });
   });
 
